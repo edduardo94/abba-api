ed38bac462b80cad385e5db70d89eaa3db6f6005
Eduardo Souza <edduardo94@gmail.com>
2021-06-19 16:25:32 -0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "Admin", "email: admin@shopmix.com.br", password: "12345678", cpf: "000000000191", user_type: 0)
